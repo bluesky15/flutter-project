ef4faa437503bc6466061f1171c09846ec1bf2ff
lalit <lalit143.er@gmail.com>
2022-01-08 23:08:48 +0530

    counter app created using cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,54 @@
-import 'package:bloc/bloc.dart';
-
-void main(List<String> args) {
-  final cubit = CounterCubit(0);
-  print(cubit.state);
-  cubit.increment();
-  print(cubit.state);
-  cubit.increment();
-  print(cubit.state);
-  cubit.close();
-}
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<int> {
   CounterCubit(int initialState) : super(initialState);
 
   void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (context) => CounterCubit(0),
+        child: const CounterPage(),
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, count) => Center(child: Text('$count')),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
 }
